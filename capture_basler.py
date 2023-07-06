299301853b2264b857f439beb796ffac85e4b288
amissadai.pinheiro <amissadai.pinheiro@conecthus.org.br>
2023-07-06 15:08:44 -0400

    Mensagem do commit

--- capture_basler.py
+++ capture_basler.py
@@ -1,43 +1,26 @@
 from pypylon import pylon
-import cv2
 
-# Inicializar o sistema Pylon
-pylon.PylonInitialize()
+# Identificando a câmera
+tl_factory = pylon.TlFactory.GetInstance()
+devices = tl_factory.EnumerateDevices()
 
-# Criar um objeto de câmera
-camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+if len(devices) == 0:
+    print("Nenhuma câmera encontrada.")
+else:
+    camera = pylon.InstantCamera()
+    tl_factory = pylon.TlFactory.GetInstance()
+    camera.Attach(tl_factory.CreateDevice(devices[0]))
+    camera.Open()
 
-# Abrir a câmera
-camera.Open()
+    if camera.IsOpen():
+        print("Câmera aberta com sucesso.")
+        camera.StartGrabbing(1)
+        grab = camera.RetrieveResult(2000, pylon.TimeoutHandling_Return)
 
-# Configurar algumas opções da câmera
-camera.Width.SetValue(640)
-camera.Height.SetValue(480)
-camera.PixelFormat.SetValue("BGR8")
-camera.AcquisitionFrameRate.setValue(60)
-
-# Criar um objeto GrabResult para receber os frames da câmera
-grabResult = pylon.GrabResult()
-
-# Loop principal
-while camera.IsGrabbing():
-    # Acionar a captura de imagem
-    camera.RetrieveResult(5000, grabResult, pylon.TimeoutHandling_ThrowException)
-    
-    # Verificar se o frame foi capturado com sucesso
-    if grabResult.GrabSucceeded():
-        # Converter o resultado para uma imagem do OpenCV
-        img = grabResult.Array
+        if grab.GrabSucceeded():
+            img = grab.GetArray()
+            print(f'Tamanho da imagem: {img.shape}')
         
-        # Exibir o frame capturado
-        cv2.imshow('Camera Basler', img)
-    
-    # Verificar se a tecla 'q' foi pressionada para encerrar o programa
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Liberar recursos
-camera.Close()
-camera.DestroyDevice()
-cv2.destroyAllWindows()
-pylon.PylonTerminate()
+        camera.Close()
+    else:
+        print("Falha ao abrir a câmera.")
